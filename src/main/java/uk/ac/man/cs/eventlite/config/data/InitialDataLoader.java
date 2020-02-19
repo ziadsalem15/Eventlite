6c62fe662c3bd8efee7af6b18aedb42766a6ac6b
Arthur Funnell <arthur.funnell@student.manchester.ac.uk>
2020-02-19 12:20:36 +0000

    Deleting events.json and editing Initial Data Loader to contain all 3 events

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -1,5 +1,8 @@
 package uk.ac.man.cs.eventlite.config.data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Component
 @Profile({ "default", "test" })
@@ -29,6 +33,13 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 		if (eventService.count() > 0) {
 			log.info("Database already populated. Skipping data initialization.");
 			return;
+			
+		} else 
+		{
+			eventService.save(new Event(1, LocalDate.of(2020, 05, 11), LocalTime.of(15, 00), "COMP23412 Showcase, group G", 1));
+			eventService.save(new Event(2, LocalDate.of(2020, 05, 05), LocalTime.of(10, 00), "COMP23412 Showcase, group H", 1));
+			eventService.save(new Event(3, LocalDate.of(2020, 05, 07), LocalTime.of(11, 00), "COMP23412 Showcase, group F", 1));
+			
 		}
 
 		// Build and save initial models here.
