9d412696ae32ae526e98278ffa58caa381884a25
r23791hc <harry.cramp@student.manchester.ac.uk>
2020-02-20 16:53:43 +0000

    Merge linking events and venues with master branch

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
@@ -37,6 +37,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import uk.ac.man.cs.eventlite.EventLite;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
@@ -81,7 +82,7 @@ public class EventsControllerApiTest {
 		e.setName("Event");
 		e.setDate(LocalDate.now());
 		e.setTime(LocalTime.now());
-		e.setVenue(0);
+		e.setVenue(new Venue());
 		when(eventService.findAll()).thenReturn(Collections.<Event>singletonList(e));
 
 		mvc.perform(get("/api/events").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
