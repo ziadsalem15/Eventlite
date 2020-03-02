c80bba2b809645771edf01edd6992185f1057684
Rumaan <rumaan.nasir@student.manchester.ac.uk>
2020-03-02 20:52:57 +0000

    Edited the controller to link addevents page with attributes

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,14 +1,21 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -28,5 +35,23 @@ public class EventsController {
 
 		return "events/index";
 	}
+	
+	@RequestMapping(value = "/addevents", method = RequestMethod.GET)
+	public String addEvents(
+			@RequestParam(value="name", required = true) String name,
+			@RequestParam(value="venue", required = true) long venue,
+			@RequestParam(value="date", required = true) LocalDate date,
+			@RequestParam(value="time", required = true) LocalTime time,
+			Model model) {
+	
+		eventService.save(new Event(name, venue, date, time));
+		model.addAttribute("events", eventService.findAll());
+		return "events/index";
+			
+	}
+
+		
+	
+	
 
 }
