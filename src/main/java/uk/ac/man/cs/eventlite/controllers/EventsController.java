4438c3b8670e7d9f50fe0b0c12f67f0848a4eeaf
Rumaan <rumaan.nasir@student.manchester.ac.uk>
2020-03-03 17:11:25 +0000

    Fixed errors present in controller and index webpage

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -3,19 +3,25 @@ package uk.ac.man.cs.eventlite.controllers;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
-import uk.ac.man.cs.eventlite.entities.Venue;
+
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -36,7 +42,7 @@ public class EventsController {
 		return "events/index";
 	}
 	
-	@RequestMapping(value = "/addevents", method = RequestMethod.GET)
+/*	@RequestMapping(value = "/addevents", method = RequestMethod.GET)
 	public String addEvents(
 			@RequestParam(value="name", required = true) String name,
 			@RequestParam(value="venue", required = true) long venue,
@@ -46,12 +52,31 @@ public class EventsController {
 	
 		eventService.save(new Event(name, venue, date, time));
 		model.addAttribute("events", eventService.findAll());
-		return "events/index";
+		return "events/addevents";
 			
 	}
-
+	
+*/
+	
+	@RequestMapping(value = "/addevents", method = RequestMethod.GET)
+	public String newEvent(Model model, HttpServletRequest request ) {
 		
-	
-	
 
+		return "events/addevents";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String addEvent(@RequestBody @Valid @ModelAttribute Event event,
+			BindingResult errors, Model model, RedirectAttributes redirectAttrs) {
+
+		if (errors.hasErrors()) {
+			model.addAttribute("event", event);
+			return "events/index";
+		}
+
+		eventService.save(event);
+		redirectAttrs.addFlashAttribute("ok_message", "New event added.");
+
+		return "redirect:/events";
+	}
 }
