c3319a51c21da8c58206744216262d954ffb724f
q28836sg <shaurya.gautam@student.manchester.ac.uk>
2020-04-20 03:03:51 +0100

    Created a find by id method in venue service implementation
    Helper method created. To be used in viewing venu information on another
    page.

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
@@ -1,16 +1,15 @@
 package uk.ac.man.cs.eventlite.dao;
+import uk.ac.man.cs.eventlite.entities.*;
 
-
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import uk.ac.man.cs.eventlite.entities.Venue;
-
 @Service
 public class VenueServiceImpl implements VenueService {
 	
+	private EventRepository eventRepository;
 	@Autowired
 	private VenueRepository venueRepository;
 
@@ -35,5 +34,9 @@ public class VenueServiceImpl implements VenueService {
 	public Iterable<Venue> listVenueByName(String name){
 		return venueRepository.findByNameContainingOrderByNameAsc(name);
 	}
-	
+	public Event getVenueByID(long id)
+	{
+		Event event = eventRepository.findById(id).get();
+		return event;	
+	}
 }
