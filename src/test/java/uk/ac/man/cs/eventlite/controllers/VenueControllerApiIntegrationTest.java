50c589ffcf08db35a386fd86b4dcbeac2e177b4a
b95983rn <rumaan.nasir@student.manchester.ac.uk>
2020-05-05 15:27:24 +0100

    Created method to test list of next 3 events in venue and created string expected response

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenueControllerApiIntegrationTest.java
@@ -0,0 +1,79 @@
+package uk.ac.man.cs.eventlite.controllers;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import uk.ac.man.cs.eventlite.EventLite;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = EventLite.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class VenueControllerApiIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests  {
+	
+	private HttpEntity<String> httpEntity;
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@BeforeEach
+	public void setup() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+		httpEntity = new HttpEntity<String>(headers);
+	}
+
+	@Test
+	public void testVenue() {
+		
+		String expectedResult = "\"name\" : \"Kilburn G23\",\n"+ 
+		"\"address\" : \"Kilburn Building, Oxford Rd, Manchester, M13 9PL\",\n" +
+		"\"capacity\" : 80,\",\n" +
+		"\"longitude\" : 0,\",\n" +
+		"\"latitude\" : 0,\",\n" +
+		"\"_links\" : {\n" +
+			"\"self\" : {\n" +
+				"\"href\" : \"http://localhost:8080/api/venues/1\"\n" +
+			"},\n" +
+			"\"venue\" : {\n" +
+				"\"href\" : \"http://localhost:8080/api/venues/1\"\n" +
+			"},\n" +
+			"\"events\" : {\n" +
+				"\"href\" : \"http://localhost:8080/api/venues/1/events\"\n" +
+			"},\n" +
+			"\"next3events\": {\n" +
+				"\"href\": \" http://localhost:8080/api/venues/1/next3events\"\n" +
+			"}\n" +
+		"}\n";
+	
+		
+		
+		
+
+	}
+	
+	
+	
+	
+	
+}
